7695cefa39d9a94b36e480309bb1a8214c9650dd
Kosmas Valianos <kosmas.valianos@appgate.com>
2022-09-20 09:56:33 +0200

    Change to Apache License 2.0

--- jsonrpc.go
+++ jsonrpc.go
@@ -1,20 +1,16 @@
-/*
- * gojsonrpc is Go package to parse and create JSON-RPC 2.0 requests/notifications and send JSON-RPC 2.0 responses
- * Copyright (C) 2022  Kosmas Valianos (kosmas.valianos@gmail.com)
- *
- * The gojsonrpc package is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The gojsonrpc package is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+/*  Copyright 2022  Kosmas Valianos (kosmas.valianos@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 package jsonrpc
 
